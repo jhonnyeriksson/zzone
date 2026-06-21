b88985cb80375fc436be7afd00fda6ce475651ba
Jhonny Eriksson <jhonny.eriksson@gmail.com>
2026-06-21 16:23:43 +0000

    update

--- GC/src/python/GC/main.py
+++ GC/src/python/GC/main.py
@@ -1,9 +1,38 @@
 import ncs
+import ncs.template
+
+
+class ServiceCallbacks(ncs.application.Service):
+
+    @ncs.application.Service.create
+    def cb_create(self, tctx, root, service, proplist):
+
+        self.log.info(f"Creating service {service.id}")
+
+        vars = ncs.template.Variables()
+
+        for pe in service.transport_information.options.settings.equipment_information.PE:
+
+            if pe.node == "XRv1":
+                loopback = "10.0.0.1"
+            elif pe.node == "XRv2":
+                loopback = "10.0.0.2"
+            else:
+                loopback = "0.0.0.0"
+
+            vars.add("LOOPBACK", loopback)
+
+        template = ncs.template.Template(service)
+        template.apply("GC-template", vars)
+
+        return proplist
+
 
 class Main(ncs.application.Application):
 
     def setup(self):
-        self.log.info("Python package started")
+        self.log.info("GC Python started")
+        self.register_service("GC", ServiceCallbacks)
 
     def teardown(self):
-        self.log.info("Python package stopped")
+        self.log.info("GC Python stopped")
