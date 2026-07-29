8a03b480e41d608d213ccb965d9b8e8f8927186a
Jhonny Eriksson <jhonny.eriksson@globalconnect.se>
2026-07-29 15:56:51 +0200

    update

--- CFS/python/CFS/main.py
+++ CFS/python/CFS/main.py
@@ -5,15 +5,35 @@ from ncs.application import Service
 
 class ServiceCallbacks(Service):
 
+    # Logical CFS hostname -> real NSO device name
+    DEVICE_MAP = {
+        "router1": "XRv1",
+        "router2": "XRv2",
+    }
+
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
-        hostname = service.hostname
+        logical_hostname = service.hostname
         self.log.info('CFS create(service={}, hostname={})'.format(
-            service.name, hostname))
+            service.name, logical_hostname))
 
-        # Create stacked RFS instance; RFS decides which loopbacks to push.
+        if logical_hostname not in self.DEVICE_MAP:
+            raise Exception(
+                "Unknown hostname '{}'; expected one of: {}".format(
+                    logical_hostname, ", ".join(sorted(self.DEVICE_MAP))
+                )
+            )
+
+        device_name = self.DEVICE_MAP[logical_hostname]
+        self.log.info('Mapped {} -> {}'.format(logical_hostname, device_name))
+
+        if device_name not in root.devices.device:
+            raise Exception("Mapped device {} does not exist in NSO".format(
+                device_name))
+
+        # Create stacked RFS instance with the real device name.
         rfs = root.RFS.create(service.name)
-        rfs.hostname = hostname
+        rfs.hostname = device_name
 
         return proplist
 
